6ff8309cb45c738b8c3973901a4245977da87c42
Mark Garner <johnmarkg@gmail.com>
2015-11-11 17:46:22 -0800

    checkPath

---
+++ example2.js
@@ -0,0 +1,29 @@
+
+
+
+var http = require('http');
+
+var service = 'micro1'
+
+var port;
+var server = http.createServer(function(req, res){
+    console.info(req.url)
+    res.end('service provider, ' + service + ' : ' + port)
+})
+
+server.listen(0, function(){
+    port = this.address().port
+    console.info('port: ' + port)
+
+    var register = require('./index')({
+        service: service,
+        port: port,
+        checkPath: '/up',
+        host: '127.0.0.1'
+    })
+
+    // routers.forEach(function(config){
+    //     routerRegister(port, config);
+    // })
+
+})
